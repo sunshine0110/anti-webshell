ce3385925e3246855c8db458eeea917c0496f594
R10T <85335450+sunshine0110@users.noreply.github.com>
2023-08-07 21:43:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,32 +1,53 @@
 <?php
-function deleteFileRecursive($dir, $filename, &$deletedCount, &$failedCount) {
+function deleteFileRecursive($dir, $filename, &$deletedDirs, &$failedDirs) {
+    $deleted = false;
+
     $files = scandir($dir);
     foreach ($files as $file) {
         if ($file === '.' || $file === '..') continue;
-        
+
         $filePath = $dir . DIRECTORY_SEPARATOR . $file;
-        
+
         if (is_dir($filePath)) {
-            deleteFileRecursive($filePath, $filename, $deletedCount, $failedCount);
+            if (deleteFileRecursive($filePath, $filename, $deletedDirs, $failedDirs)) {
+                $deleted = true;
+            }
         } elseif ($file === $filename) {
             if (unlink($filePath)) {
-                $deletedCount++;
-                echo "File $filename berhasil dihapus.<br>";
-            } else {
-                $failedCount++;
-                echo "Gagal menghapus file $filename.<br>";
+                $deleted = true;
             }
         }
     }
+
+    if ($deleted) {
+        $deletedDirs[] = $dir;
+    } else {
+        $failedDirs[] = $dir;
+    }
+
+    return $deleted;
 }
 
 $documentRoot = $_SERVER['DOCUMENT_ROOT'];
 $filenameToDelete = 'cok.html';
-$deletedCount = 0;
-$failedCount = 0;
+$deletedDirs = [];
+$failedDirs = [];
 
-deleteFileRecursive($documentRoot, $filenameToDelete, $deletedCount, $failedCount);
+deleteFileRecursive($documentRoot, $filenameToDelete, $deletedDirs, $failedDirs);
 
-echo "Total file berhasil dihapus: $deletedCount<br>";
-echo "Total file gagal dihapus: $failedCount<br>";
+if (!empty($deletedDirs)) {
+    echo "File $filenameToDelete berhasil dihapus dari direktori:<br>";
+    foreach ($deletedDirs as $deletedDir) {
+        echo "- $deletedDir<br>";
+    }
+} else {
+    echo "Gagal menghapus file $filenameToDelete dari semua direktori.<br>";
+}
+
+if (!empty($failedDirs)) {
+    echo "Gagal menghapus file $filenameToDelete dari direktori berikut:<br>";
+    foreach ($failedDirs as $failedDir) {
+        echo "- $failedDir<br>";
+    }
+}
 ?>
